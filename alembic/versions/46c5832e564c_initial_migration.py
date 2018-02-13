8278a723415cf9b569a4b3da70fb4d7cac967965
boberstarosta <boberstarosta@gmail.com>
2018-02-13 17:44:35 +0100

    Initial migration

---
+++ alembic/versions/46c5832e564c_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: 46c5832e564c
+Revises: 
+Create Date: 2018-02-13 17:38:27.625860
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '46c5832e564c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
