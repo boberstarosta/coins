87f9d70f8647a0f7fa10d1ed4297e4c384052f90
boberstarosta <boberstarosta@gmail.com>
2018-02-13 19:10:52 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='coins',
+    version='0.1',
+    description='Fresh coin prices',
+    author='boberstarosta',
+    author_email='boberstarosta@gmail.com',
+    packages=['coins'],
+    zip_safe=False
+)
