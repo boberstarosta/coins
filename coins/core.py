8a7180b97a6ee110339641cc87fc3494c0bf13f8
boberstarosta <boberstarosta@gmail.com>
2018-02-13 19:31:10 +0100

    Created get_stooq_price()

---
+++ coins/core.py
@@ -0,0 +1,18 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+def get_stooq_price(name):
+    name = name.lower()
+    url = 'https://stooq.pl/q/?s=' + name
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    search_phrase = 'aq_{name}_c[0-9]'.format(name=name)
+    tag = soup.find('span', id=re.compile(search_phrase))
+    if tag is None:
+        return None
+    else:
+        try:
+            return float(tag.get_text())
+        except ValueError:
+            return None
