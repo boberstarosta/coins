920adafc2a7d894bd69e46cb7168d931b68d72d7
boberstarosta <boberstarosta@gmail.com>
2018-02-13 21:50:51 +0100

    Updated core arg

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from coins import core, db, models
 
 if __name__ == "__main__":
     print('Updating prices...')
-    core.update_metal_prices()
     session = db.Session()
+    core.update_metal_prices(session)
     metals = session.query(models.Metal).all()
     print('Latest prices:')
     for metal in metals:
