b0b81571c5331cf9f64e10a3e31d7f3ae9422610
boberstarosta <boberstarosta@gmail.com>
2018-02-13 17:27:22 +0100

    Created test run

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
-import sqlalchemy
+from coins import db, models
 
 
 if __name__ == "__main__":
-    print("SQLAlchemy version: ", sqlalchemy.__version__)
+    models.Base.metadata.create_all(db.engine)
+    session = db.get_session()
+    m = models.Metal(name='test_met_1')
+    session.add(m)
+    session.commit()
+    print(m.id)
+
